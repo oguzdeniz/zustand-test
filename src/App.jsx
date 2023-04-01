6295fba00fe1ac4a2dcf278666d640b8b73227c9
oguzdeniz <ahmetsert@gmail.com>
2023-04-01 17:53:47 +0300

    base to vite.config added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Users from './components/Users'
 
 export default function App() {
+  console.log(import.meta.env.VITE_TEST)
+  console.log(import.meta.env.VITE_KEY)
   return (
     <div>
       <Users />
